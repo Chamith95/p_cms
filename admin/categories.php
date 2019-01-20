6706daf2dcf9cae661b078bebbb26b93f6c503ca
Chamith95 <chamithjay@gmail.com>
2019-01-21 01:45:40 +0530

    admin categories

--- admin/categories.php
+++ admin/categories.php
@@ -17,30 +17,7 @@
                         <small>Author</small>
                     </h1>
                     <div class="col-xs-6">
-                        <?php 
-
-                        if(isset($_POST['submit'])){
-                            $cat_title=$_POST['cat_title'];
-
-                            if($cat_title== ""|| empty($cat_title)){
-
-                                echo "This field should not be empty";
-                            }
-                            else{
-
-                                $query="INSERT INTO categories(cat_title) ";
-                                $query .="VALUE('{$cat_title}')";
-
-                                $create_category_query=mysqli_query( $connection, $query);
-
-                                if(!$create_category_query){
-                                    die('QUERY FAILED'.mysqli_error($connection));
-                                }
-                            }
-
-
-                        }
-?>
+                        <?php insert_categories();?>
 
                         <form action="" method="post">
                             <div class="form-group">
@@ -52,16 +29,19 @@
                             </div>
 
                         </form>
+                        <?php //update categories
+                        if(isset($_GET['edit'])){
+                            $cat_id=$_GET['edit'];
+
+                            include "includes/update_categories.php";
+
+                        }
+                        ?>
                     </div>
 
                     <div class="col-xs-6">
 
-                        <?php
-                            $query="SELECT * FROM categories";
-                            $select_categories=mysqli_query($connection,$query);
-                    
-                    
-                                    ?>
+
 
                         <table class="table table-bordered table-hover">
                             <thead>
@@ -71,23 +51,12 @@
                                 </tr>
                             </thead>
                             <tbody>
-                            
-                                <?php
 
-                    while($row=mysqli_fetch_assoc( $select_categories)){
-                            $cat_id=$row['cat_id'];
-                            $cat_title=$row['cat_title'];
+                                <?php findAllcategories();?>
 
-                            echo "<tr>";
-                            echo "<td>{$cat_id}</td>";
-                            echo "<td>{$cat_title}</td>";
-                            echo "</tr>";  
+                                <?php deleteCategories();?>
 
-}
 
-                    ?>
-                                
-                                
 
                             </tbody>
                         </table>
--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,11 +1,12 @@
+<?php ob_start()?>
 <?php include "../includes/db.php"  ?>
-
+<?php include "functions.php"  ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
 
-    <meta charset="utf-8">
+    <meta charset="utf-8"> 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
